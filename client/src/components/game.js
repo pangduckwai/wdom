cd5cb4a5983eb439b9e682a8f4ad34253a3b07da
pangduckwai <paul.lai@sea9.org>
2019-07-13 11:45:49 +0800

    Lift state up

--- client/src/components/game.js
+++ client/src/components/game.js
@@ -9,6 +9,7 @@ export default class Game extends React.Component {
 		this.state = {
 			round: 0,
 			selected: "",
+			focused: "",
 			players: [],
 			owners: []
 		};
@@ -35,28 +36,57 @@ export default class Game extends React.Component {
 			);
 	}
 
-	handleClick(value) {
-		this.setState({selected: value});
+	handleHover(value, e) {
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+
+		this.setState({
+			focused: value
+		});
+	}
+
+	handleClick(value, e) {
+		e.stopPropagation();
+		e.nativeEvent.stopImmediatePropagation();
+
+		this.setState({
+			selected: value,
+			focused: value
+		});
 	}
 
 	handleClear() {
 		this.setState({selected: ""});
 	}
 
+	handleUnhover() {
+		this.setState({focused: ""});
+	}
+
 	render() {
 		const curr = (this.state.selected !== "") ? LINK[this.state.selected].connected : [];
 
 		return (
-			<svg className="app-game fillv map-game" viewBox="0 0 1227 628" onClick={this.handleClear.bind(this)}>
+			<svg className="app-game fillv map-game" viewBox="0 0 1227 628"
+				onClick={this.handleClear.bind(this)}
+				onMouseOver={this.handleUnhover.bind(this)}>
+
 				{LINE.map((points, i) =>
 					<line key={i} x1={points[0]} y1={points[1]} x2={points[2]} y2={points[3]} />)}
+
 				{Object.keys(MAP).map((key) =>
 					(<Territory
 						key={key} tid={key}
 						player={(this.state.owners[key] != null) ? this.state.owners[key] : 0}
+						army={(this.state.owners[key] != null) ? 1 : 0}
 						sel={key === this.state.selected}
 						lnk={curr.includes(key)}
-						onClick={this.handleClick.bind(this, key)} />))}
+						onClick={this.handleClick.bind(this, key)}
+						onMouseOver={this.handleHover.bind(this, key)} />))}
+
+				<text className="tname" x="560" y="590">
+					{(this.state.selected === "") ? this.state.focused : this.state.selected}
+				</text>
 			</svg>
 		);
 	}
--- client/src/components/territory.js
+++ client/src/components/territory.js
@@ -2,45 +2,26 @@ import React from 'react';
 import { MAP } from './constants';
 import './map.css';
 
-export default class Territory extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			army: 1
-		};
-	}
+export default function Territory(props) {
+	const local = MAP[props.tid];
+	let clazz = 'c';
+	if (props.sel) clazz = 'c s';
+	if (props.lnk) clazz = 'c l';
 
-	handleClick(value, e) {
-		e.stopPropagation();
-		e.nativeEvent.stopImmediatePropagation();
+	return (
+		<g className={clazz} id={props.tid}
+			onClick={props.onClick}
+			onMouseOver={props.onMouseOver}>
 
-		this.props.onClick(value);
-		this.setState((state, props) => ({
-			army: state.army + 1
-		}));
-	}
-
-	render() {
-		const local = MAP[this.props.tid];
-		let clazz = 'c';
-		if (this.props.sel) clazz = 'c s';
-		if (this.props.lnk) clazz = 'c l';
-
-		return (
-			<g className={clazz} id={this.props.tid} onClick={this.handleClick.bind(this, this.props.tid)}>
-				<path
-					className={`${local.continent} ${local.cindex}`}
-					d={local.svgPath}/>
-				<text className="tname" x="560" y="590">
-					{this.props.tid}
-				</text>
-				<text className={`tarmy ${this.props.player <= 0 ? "hide" : "show"}`} x={local.loc[0]} y={local.loc[1]}>
-					{this.state.army}
-				</text>
-				<polyline
-					className={`player${this.props.player}`} points="0,0 0,-20 20,-15 0,-10"
-					transform={`translate(${local.loc[2]}, ${local.loc[3]})`} />
-			</g>
-		);
-	}
+			<path
+				className={`${local.continent} ${local.cindex}`}
+				d={local.svgPath}/>
+			<text className={"tarmy"} x={local.loc[0]} y={local.loc[1]}>
+				{props.army}
+			</text>
+			<polyline
+				className={`player${props.player}`} points="0,0 0,-20 20,-15 0,-10"
+				transform={`translate(${local.loc[2]}, ${local.loc[3]})`} />
+		</g>
+	);
 }
